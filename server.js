4fec79cc6b0e13acfe69e5ed52c1b3628b5dd350
Ravi Mantur <RaviMantur@RavisMac.local>
2019-08-26 18:39:39 -0400

    created code for server.js( starting point for node/express server).

---
+++ server.js
@@ -0,0 +1,35 @@
+//=========================================================================
+//server.js-This is the initial starting point for the node/express server.
+//=========================================================================
+//dependicies
+
+var express = require("express");
+
+//sets up express app
+
+var app = express();
+var port = process.env.port || 7500;
+
+//requiring our models for syncing
+
+var db = require("./models");
+
+//sets up the express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static directory
+app.use(express.static("public"));
+
+//routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+//syncing our sequlize models and then starting our express app
+
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(port, function () {
+        console.log("App listening on port" + port);
+    });
+});
