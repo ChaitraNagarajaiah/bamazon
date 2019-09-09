d740ae527130dbe78c475e1153cac4319b32713f
Ravi Mantur <RaviMantur@RavisMac.local>
2019-09-09 14:47:24 -0400

    added .env file

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "Ravi@07Mobile",
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.PASSWORD,
     "database": "bamazon",
     "host": "127.0.0.1",
     "dialect": "mysql",
